295929354f0f4349a5a7798fecdaee1c4261db31
Salil <salil.joshi@alpha-grep.com>
2015-07-03 10:40:35 +0530

    install.sh ln fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf ~/.vimrc
 rm -rf ~/.vim
-ln -sf vim/.vimrc ~/.vimrc 
-ln -sf vim/.vimrc ~/.vim 
+ln -sf `pwd`/vim/.vimrc ~/.vimrc 
+ln -sf `pwd`/vim/.vimrc ~/.vim 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neobundleinstall.sh
 sh ./neobundleinstall.sh
